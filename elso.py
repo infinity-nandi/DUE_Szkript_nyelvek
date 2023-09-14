452af9ae2251dc15f0d946d9ce9e0fd423bf9586
infinity-nandi <nandorrexexel@gmail.com>
2023-09-14 10:32:24 +0200

    print parancsok

--- elso.py
+++ elso.py
@@ -1,2 +1,10 @@
-print("Hello Világ")
-print("Nem")
+print("Hello \nVilág", end="\n")
+print("Szia", "Józsi", "bácsi", sep="-----", end="\n\n")
+
+nev = input("Mi a neved: ")
+
+print(f"Szia", nev)
+print("Szia {0}" .format("Lala"))
+print("Szöveg".rjust(50, "*"))
+print("Szöveg".ljust(50, "+"))
+print("Szöveg".center(50, "?"))
